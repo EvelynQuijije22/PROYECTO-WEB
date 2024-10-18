d637fb5136baa35fb233d18c5c60b17beef5e9dc
Sander2106 <0992001802s@gmail.com>
2024-10-17 22:48:15 -0500

    Update scripts.js

--- proyecto/Js/scripts.js
+++ proyecto/Js/scripts.js
@@ -1,55 +1,54 @@
-// Validación de inicio de sesión
+
 document.getElementById('loginForm')?.addEventListener('submit', function validarLogin(event) {
-  event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+  event.preventDefault(); 
 
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
   if (!email || !password) {
     alert("Todos los campos son obligatorios.");
-    return false; // Evitar el envío del formulario
+    return false; 
   }
 
   if (!validarEmail(email)) {
     alert("Por favor, introduce un email válido.");
-    return false; // Evitar el envío del formulario
+    return false; 
   }
 
-  // Alerta de inicio de sesión exitoso
   ("Inicio de sesión exitoso");
   
-  return false; // Permitir el envío si es necesario
+  return false; 
 });
 
-// Función de validación de email
+
 function validarEmail(email) {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(String(email).toLowerCase());
 }
 
 
-// Validación de registro de usuario
+
 document.getElementById('signupForm')?.addEventListener('submit', function validarRegistro(event) {
-  event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+  event.preventDefault(); 
 
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value.trim();  // Eliminar espacios en blanco
-  const confirmPassword = document.getElementById('confirmPassword').value.trim();  // Eliminar espacios en blanco
+  const password = document.getElementById('password').value.trim();  
+  const confirmPassword = document.getElementById('confirmPassword').value.trim();  
 
-  // Verificar que todos los campos estén completos
+ 
   if (!name || !email || !password || !confirmPassword) {
     alert("Todos los campos son obligatorios.");
-    return false; // Evitar el envío del formulario
+    return false; 
   }
 
-  // Validar que el correo electrónico sea válido
+ 
   if (!validarEmail(email)) {
     alert("Por favor, introduce un email válido.");
-    return false; // Evitar el envío del formulario
+    return false; 
   }
 
-  // Validar que las contraseñas coincidan
+ 
   if (password !== confirmPassword) {
     alert("Las contraseñas no coinciden.");
     return false; // Evitar el envío del formulario
